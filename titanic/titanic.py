db1d3993cd28d885f2135ac4c5b3509b5f6b2c0a
nyoung1 <nolen.young@wsu.edu>
2020-06-09 20:38:14 -0700

    First model working properly.

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -2,38 +2,58 @@
 # titanic.py
 # Author: Nolen Young
 # Last modified: 6-9-2020
-# Description: This file is the main file for my kaggle titanic competition solution. The file imports data about
+# Description: This file is the main file for my kaggle titanic competition solution. This includes simple driver code
+# to take in command line arguments and run the program. The file imports data about
 # titanic passengers and trains a machine learning model that will try to predict which passengers may have lived
 # or perished. Link: https://www.kaggle.com/c/titanic/
 #######################################################################################################################
 
-
+import pandas as pd
 import sys
 import os
+from sklearn.ensemble import RandomForestClassifier
 
-from learn import startLearning
-
+features = ["Pclass", "Sex", "SibSp", "Parch"]
 
 # Main
-def main(mode, inFilePath):
-    if mode == '-learn':
-        startLearning(inFilePath)
-    elif mode == '-test':
-        return 0
-    else:
-        print("Error: Invalid first argument, {}. First argument must be either -learn or -test.")
+def main(trainFilePath, testFilePath):
+    # read data from inFile into trainData
+    try:
+        trainData = pd.read_csv(trainFilePath)
+        testData = pd.read_csv(testFilePath)
+    except pd.errors.EmptyDataError as err:  # check for empty data set
+        print("Error: {}".format(err))
         return 1
+
+    model = learn(trainData)
+    results = test(testData, model)
+
+    output = pd.DataFrame({'PassengerId': testData.PassengerId, 'Survived': results})
+    output.to_csv('results.csv', index=False)
+
     return 0
+
+
+# runs the machine learning on
+def learn(trainData):
+    Y = trainData["Survived"]
+    X = pd.get_dummies(trainData[features])
+    model = RandomForestClassifier(n_estimators=1000, max_depth=50, random_state=1)
+    return model.fit(X, Y)
+
+def test(testData, model):
+    X_test = pd.get_dummies(testData[features])
+    return model.predict(X_test)
 
 
 # parse command line args
 if __name__ == "__main__":
     if len(sys.argv) == 3:  # must have two commands
-        mode = sys.argv[1]
-        inFilePath = sys.argv[2]
+        trainFilePath = sys.argv[1]
+        testFilePath = sys.argv[2]
 
-        if os.path.isfile(inFilePath): # check if file exists
-            if not main(mode, inFilePath):  # RUN MAIN
+        if os.path.isfile(trainFilePath) and os.path.isfile(testFilePath): # check if file exists
+            if not main(trainFilePath, testFilePath):  # RUN MAIN
                 print("Done.")
                 exit(0)
             else:
@@ -43,7 +63,7 @@ if __name__ == "__main__":
             print("Error: File does not exists.")
             exit(1)
     else:
-        print("Error: Invalid number of command line arguments. 2 arguments required.")
+        print("Error: Invalid number of command line arguments. 3 arguments required.")
         exit(1)
 else:
     print("Error: Improper usage of titanic.py")
